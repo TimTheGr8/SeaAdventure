90a51b1ccbaf1c84205b51cf15c11e9e34aff68f
Tim Janssen <timothyjanssen4@gmail.com>
2024-06-04 13:55:22 -0500

    Started adding enemies.

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -1,9 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField]
     private EnemyConfigData _config;
+
+    [SerializeField]
+    private enum State
+    {
+        Idle, Patrol, Attack, Dead
+    }
+
+    private State _currentState;
+
+    private void Start()
+    {
+        ChangeState(State.Idle);
+    }
+
+    private void Update()
+    {
+        switch (_currentState)
+        {
+            case State.Idle:
+                break;
+            case State.Patrol:
+                break;
+            case State.Attack:
+                break;
+            case State.Dead:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ChangeState(State state)
+    {
+        _currentState = state;
+    }
 }
