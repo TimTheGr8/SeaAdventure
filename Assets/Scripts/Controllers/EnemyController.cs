dee8f9a18a0019bc4a10d730e1f9dd5c41741841
Tim Janssen <timothyjanssen4@gmail.com>
2025-02-22 18:16:06 -0600

    Fixed the dinghy functionality.

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField]
     private EnemyConfigData _config;
 
-    [SerializeField]
-    private enum State
+    public enum State
     {
         Idle, Patrol, Attack, Dead
     }
 
     private State _currentState;
+    private float _timeStateStart;
+    private Rigidbody2D _rb;
+    private Vector2 _movementDirection;
+    private IBehaviorPatrolWaypoints _behaviorPatrol;
+    private GameObject _player;
+
+    private void Awake()
+    {
+        if(TryGetComponent<IBehaviorPatrolWaypoints>(out _behaviorPatrol))
+        {
+            _behaviorPatrol.Init(_rb, _movementDirection, _config._acceleration, _config._speedMax);
+        }
+        _player = GameObject.FindWithTag(Tags.Player);
+    }
 
     private void Start()
     {
@@ -26,10 +38,24 @@ public class EnemyController : MonoBehaviour
         switch (_currentState)
         {
             case State.Idle:
+                if(IsPlayerInRange(_config._attackRange))
+                {
+                    ChangeState(State.Attack);
+                }
+                else if (Time.time - _timeStateStart >= _config._timePatrol)
+                    ChangeState(State.Patrol);
                 break;
             case State.Patrol:
+                if (IsPlayerInRange(_config._attackRange))
+                {
+                    ChangeState(State.Attack);
+                }
+                else if (Time.time - _timeStateStart >= _config._timePatrol)
+                    ChangeState(State.Idle);
                 break;
             case State.Attack:
+                if(!IsPlayerInRange(_config._attackRange))
+                    ChangeState(State.Patrol);
                 break;
             case State.Dead:
                 break;
@@ -38,8 +64,24 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    private void ChangeState(State state)
+    private void FixedUpdate()
+    {
+        if(_currentState == State.Patrol)
+            _behaviorPatrol?.TickPhysics();
+        else
+            _rb.velocity = Vector2.zero;
+    }
+
+    public void ChangeState(State state)
     {
         _currentState = state;
+        _timeStateStart = Time.time;
+    }
+
+    private bool IsPlayerInRange(float rangeAttack)
+    {
+        var distance = Vector2.Distance(transform.position, _player.transform.position);
+        
+        return distance <= rangeAttack;
     }
 }
